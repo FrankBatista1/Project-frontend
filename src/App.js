c84d0975eab4915c2338caef8ef8a5f4f79473f8
FrankBatista1 <fb53901@gmail.com>
2021-10-04 15:07:48 -0400

    Home deleted for test deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import './App.css';
-import Home from './views/Home';
 import LoginPage from './views/LoginPage'
 import SignUpPage from './views/SignUpPage'
 import WorkoutViews from './views/SingleDayView';
@@ -12,8 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Home}></Route>
-        <Route exact path="/login" component={LoginPage}></Route>
+        <Route exact path="/" component={LoginPage}></Route>
         <Route exact path="/signup" component={SignUpPage}></Route>
         <UserView exact path="/calendar" component={MyPlanner}></UserView>
         <Route exact path="/singleday" component={WorkoutViews}></Route>
