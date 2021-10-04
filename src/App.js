2633c4605fa3b763fd866e7a1f43f3659b22f4fd
FrankBatista1 <fb53901@gmail.com>
2021-10-04 15:11:56 -0400

    changed the tailwind config file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import './App.css';
+import Home from './views/Home';
 import LoginPage from './views/LoginPage'
 import SignUpPage from './views/SignUpPage'
 import WorkoutViews from './views/SingleDayView';
@@ -11,7 +12,8 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={LoginPage}></Route>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/login" component={LoginPage}></Route>
         <Route exact path="/signup" component={SignUpPage}></Route>
         <UserView exact path="/calendar" component={MyPlanner}></UserView>
         <Route exact path="/singleday" component={WorkoutViews}></Route>
