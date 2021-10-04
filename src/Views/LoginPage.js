a5d48a9d9679754e8384edd3c314b670492a4924
FrankBatista1 <fb53901@gmail.com>
2021-10-03 23:29:33 -0400

    Home page and login styles added

--- src/Views/LoginPage.js
+++ src/Views/LoginPage.js
@@ -1,11 +1,10 @@
-
 import { Link } from "react-router-dom";
-import {useState, useContext, useEffect} from 'react'
+import { useState, useContext, useEffect } from "react";
 import axios from "axios";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
-const LoginPage = ({history}) => {
-  const apiUrl = process.env.REACT_APP_API_URL
+const LoginPage = ({ history }) => {
+  const apiUrl = process.env.REACT_APP_API_URL;
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -42,34 +41,122 @@ const LoginPage = ({history}) => {
     }
   };
   return (
-    <div>
-      <form onSubmit={loginHandler}>
-        <label>email</label>
-        <input
-          onChange={(e) => setEmail(e.target.value)}
-          name="email"
-          value={email}
-          type="text"
-          placeholder="Email"
-        />
-        <label>password</label>
-        <input
-          onChange={(e) => setPassword(e.target.value)}
-          name="password"
-          value={password}
-          type="password"
-          placeholder="Password"
-        />
-      <button type="submit">Login</button>
-      {error && <span className="error-message">{error}</span>}
-      </form>
-      <Link to="/signup">Doesn't have an account?</Link>
-      <br/>
-      <Link to="/">Home</Link>
-      <br/>
-      <Link to="/calendar">Fake login to test</Link>
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <img
+            className="mx-auto h-12 w-auto"
+            src="https://res.cloudinary.com/duscflsvf/image/upload/v1633316619/International_Marathon_Running_Shoe_Logo_2_xznkd8.png"
+            alt="Workflow"
+          />
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Sign in to your account
+          </h2>
+          <p className="mt-2 text-center text-sm text-gray-600">
+          {error ? <span style={{color: 'rgb(209, 2, 20)'}}>{error}</span> : <span>Please enter your credentials</span>}
+          </p>
+        </div>
+        <form onSubmit={loginHandler} className="mt-8 space-y-6">
+          <input type="hidden" name="remember" defaultValue="true" />
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="email-address" className="sr-only">
+                Email address
+              </label>
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                name="email"
+                value={email}
+                id="email-address"
+                type="email"
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                name="password"
+                value={password}
+                type="password"
+                placeholder="Password"
+                id="password"
+                autoComplete="current-password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 focus:z-10 sm:text-sm"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                className="h-4 w-4 text-yellow-600 focus:ring-yellow-500 border-gray-300 rounded"
+              />
+              <label
+                htmlFor="remember-me"
+                className="ml-2 block text-sm text-gray-900"
+              >
+                Remember me
+              </label>
+            </div>
+
+            <div className="text-sm">
+              <Link to="#"
+                className="font-medium text-yellow-600 hover:text-yellow-500"
+              >
+                Forgot your password?
+              </Link>
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
-  }
+};
 
 export default LoginPage;
+
+// eslint-disable-next-line no-lone-blocks
+{
+  /* <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <form className="mt-8 space-y-6" onSubmit={loginHandler}>
+            <div className="rounded-md shadow-sm -space-y-px">
+              <label>email</label>
+              <input
+                
+              />
+              <label>password</label>
+              <input
+               
+              />
+              <button type="submit">Login</button>
+              {error && <span className="error-message">{error}</span>}
+            </div>
+          </form>
+          <Link to="/signup">Doesn't have an account?</Link>
+          <br />
+          <Link to="/">Home</Link>
+          <br />
+          <Link to="/calendar">Fake login to test</Link>
+        </div>
+      </div>
+    </div> */
+}
